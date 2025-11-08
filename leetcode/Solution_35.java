909919720da0efc811cf2c31d41b6d70dab7eb22
AtamjonRaximov <raximovatam999@gmail.com>
2025-11-08 10:58:07 +0500

    next solution35

---
+++ leetcode/Solution_35.java
@@ -0,0 +1,33 @@
+package pdpuz;
+
+import java.util.HashMap;
+
+public class Solution_35 {
+    public static void main(String[] args) {
+        Solution_35 sol1 = new Solution_35();
+
+        int[] arr = {1, 3, 5, 6};
+        int target = 7;
+        System.out.println(sol1.searchInsert(arr, target));
+
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+
+    }
+}
